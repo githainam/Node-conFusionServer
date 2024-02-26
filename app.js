fed3fc84b380855fcbf334cc01a87df39bc6412c
githainam <hainamrin230802@gmail.com>
2024-02-26 14:09:25 +0700

    Authenticate basic

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var session = require("express-session");
+var FileStore = require("session-file-store")(session);
+var passport = require('passport');
+var authenticate = require('./authenticate');
 
 // var indexRouter = require("./routes/index");
 var dishRouter = require("./routes/dishRouter");
@@ -11,6 +15,7 @@ var dishRouter = require("./routes/dishRouter");
 // var toppingRouter = require("./routes/toppingRouter");
 // var youtubeRouter = require("./routes/youtubeRouter");
 // var cakeRouter = require("./routes/cakeRouter");
+var userRouter = require("./routes/usersRouter");
 
 var app = express();
 const mongoose = require("mongoose");
@@ -27,49 +32,101 @@ app.use(express.urlencoded({ extended: false }));
 // app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
+
+
 app.use(cookieParser("18102002"));
+
+app.use(
+  session({
+    name: "session-id",
+    secret: "12345-67890-09876-54321",
+    saveUninitialized: false,
+    resave: false,
+    store: new FileStore(),
+  })
+);
 function auth(req, res, next) {
-  if (!req.signedCookies.user) {
-    var authHeader = req.headers.authorization;
-    if (!authHeader) {
-      var err = new Error("You are not authenticated!");
-      res.setHeader("WWW-Authenticate", "Basic");
-      err.status = 401;
-      next(err);
-      return;
-    }
-    var auth = new Buffer.from(authHeader.split(" ")[1], "base64")
-      .toString()
-      .split(":");
-    var user = auth[0];
-    var pass = auth[1];
-    if (user == "admin" && pass == "password") {
-      res.cookie("user", "admin", { signed: true });
-      next(); // authorized
-    } else {
-      var err = new Error("You are not authenticated!");
-      res.setHeader("WWW-Authenticate", "Basic");
-      err.status = 401;
-      next(err);
-    }
+  console.log(req.session);
+
+  if (!req.session.user) {
+    var err = new Error("You are not authenticated!");
+    err.status = 403;
+    return next(err);
   } else {
-    if (req.signedCookies.user === "admin") {
+    if (req.session.user === "authenticated") {
       next();
     } else {
       var err = new Error("You are not authenticated!");
-      err.status = 401;
-      next(err);
+      err.status = 403;
+      return next(err);
     }
   }
 }
 
+// function auth(req, res, next) {
+//   console.log(req.session);
+
+//   if (!req.session.user) {
+//     var authHeader = req.headers.authorization;
+//     if (!authHeader) {
+//       var err = new Error("You are not authenticated!");
+//       res.setHeader("WWW-Authenticate", "Basic");
+//       err.status = 401;
+//       next(err);
+//       return;
+//     }
+//     var auth = new Buffer.from(authHeader.split(" ")[1], "base64")
+//       .toString()
+//       .split(":");
+//     var user = auth[0];
+//     var pass = auth[1];
+//     if (user == "admin" && pass == "password") {
+//       req.session.user = "admin";
+//       next(); // authorized
+//     } else {
+//       var err = new Error("You are not authenticated!");
+//       res.setHeader("WWW-Authenticate", "Basic");
+//       err.status = 401;
+//       next(err);
+//     }
+//   } else {
+//     if (req.session.user === "admin") {
+//       console.log("req.session: ", req.session);
+//       next();
+//     } else {
+//       var err = new Error("You are not authenticated!");
+//       err.status = 401;
+//       next(err);
+//     }
+//   }
+// }
+
 // app.use("/", indexRouter);
+app.use("/users", userRouter);
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(auth);
+
+function auth (req, res, next) {
+  console.log(req.user);
+
+  if (!req.user) {
+    var err = new Error('You are not authenticated!');
+    err.status = 403;
+    next(err);
+  }
+  else {
+        next();
+  }
+}
+
 
 app.use("/dishes", dishRouter);
 // app.use("/toppings", toppingRouter);
 // app.use("/promotions", promotionRouter);
 // app.use("/leaders", leaderRouter);
+// app.use("/youtubes", youtubeRouter);
+// app.use("/cakes", cakeRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -97,3 +154,4 @@ connect.then(
 );
 
 module.exports = app;
+
